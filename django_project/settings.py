cb1838c23d2a08ac4821c55a032ddc06141a00c0
Prabin Karki <161217370+Prabinkarki67@users.noreply.github.com>
2025-08-21 23:05:46 +0545

    added crispy / bootstrap 5 to make ui of registration a bit good where i downloaded crispy and loaded it in register.html

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     ##'blog',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
